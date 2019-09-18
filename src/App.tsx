8adca6c72c4f739afff9ff64008e4b1486cf57b5
Ilya Blinkov <blinkovilya@yandex-team.ru>
2019-09-18 22:51:09 +0300

    AppHeader: WIP for Logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
 
+import IUser from './typing/IUser';
+
 interface IProps {
 
 };
 
 interface IState {
-
+  user: IUser | null;
 };
 
 class App extends React.Component<IProps, IState> {
   constructor(props: IProps) {
     super(props);
     this.state = {
-      
+      user: null
     };
   }
 
---
+++ src/components/AppHeader/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+interface IProps {
+
+};
+
+interface IState {
+
+};
+
+class AppHeader extends React.Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <header className="AppHeader">
+        <div className="container">
+
+        </div>
+      </header>
+    );
+  }
+};
+
+export default AppHeader;
