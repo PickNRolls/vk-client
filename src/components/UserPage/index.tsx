29a34e740b50ff4430897d6b74986463f0f10180
Ilya Blinkov <blinkovilya@yandex-team.ru>
2019-09-21 01:22:48 +0300

    UserPageAvatar: Created

--- src/components/UserPage/index.tsx
+++ src/components/UserPage/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import UserPageAvatar from '../UserPageAvatar';
+
 import IBaseProps from '../../typing/IBaseProps';
 import cn from '../../helpers/cn';
 
@@ -18,14 +20,15 @@ class UserPage extends React.Component<IProps, IState> {
   }
 
   render() {
-    const cUserPage = cn('UserPage', this.props.className);
+    const propsClass = this.props.className ? this.props.className + ' clearfix' : 'clearfix';
+    const cUserPage = cn('UserPage', propsClass);
     return (
       <div className={cUserPage}>
         <div className="page-column-thin">
-
+          <UserPageAvatar imageUrl="https://sun9-41.userapi.com/c846018/v846018774/1964b4/MTv4NbdWX0E.jpg?ava=1" />
         </div>
         <div className="page-column-wide">
-          
+
         </div>
       </div>
     );
