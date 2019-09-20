1ffee8997983abb1ddadb71ba74cab8f46061221
Ilya Blinkov <blinkovilya@yandex-team.ru>
2019-09-21 01:52:07 +0300

    UserPageInfo: WIP for UserInfoField

---
+++ src/components/UserPageInfo/index.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+import IUser from '../../typing/IUser';
+import IBaseProps from '../../typing/IBaseProps';
+import cn from '../../helpers/cn';
+
+interface IProps extends IBaseProps {
+  user: IUser;
+};
+
+interface IState {
+  expanded: boolean;
+};
+
+class UserPageInfo extends React.Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
+    this.state = {
+      expanded: false
+    };
+  }
+
+  render() {
+    const { user } = this.props;
+    const cUserPageInfo = cn('UserPageInfo', this.props.className);
+    return (
+      <div className={cUserPageInfo}>
+        <div className="UserPageInfo-Top">
+          <span className="UserPageInfo-Name">
+            {user.fullName}
+          </span>
+          <span className="UserPageInfo-Online">
+            {user.online ? 'Online' : 'Offline'}
+          </span>
+        </div>
+
+        {
+        user.status
+          ? <span className="UserPageInfo-Status">
+              {user.status}
+            </span>
+
+          : null
+        }
+
+        <div className="UserPageInfo-AlwaysVisible">
+          <span className="UserPageInfo-Field">
+            
+          </span>
+        </div>
+      </div>
+    );
+  }
+};
+
+export default UserPageInfo;
--- src/typing/IUser.ts
+++ src/typing/IUser.ts
@@ -1,9 +1,19 @@
-export default interface User {
+type TLanguage = 'Russian' | 'English' | 'German' | 'French';
+
+export interface IUserAdditionalInfo {
+  birthday: Date;
+
+  languages?: TLanguage[];
+}
+
+export default interface IUser {
   id: string;
   avatar: string;
   fullName: string;
   firstName: string;
   lastName: string;
+  status?: string;
   age: number;
   gender: boolean;
+  online: true | Date;
 };
