a7b453e443425d04a710ff4dfb2853bf4f93dc60
Ilya Blinkov <blinkovilya@yandex-team.ru>
2019-09-20 21:56:39 +0300

    AppMain: WIP for SideMenu

---
+++ src/components/AppMain/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface IProps {
+
+};
+
+const AppMain: React.FC<IProps> = props => {
+  return (
+    <main className="AppMain">
+      
+    </main>
+  );
+};
+
+export default AppMain;
