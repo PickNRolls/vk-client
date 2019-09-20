6fa057238369df43db2ecba325bd7fc68629f3e7
Ilya Blinkov <blinkovilya@yandex-team.ru>
2019-09-20 22:59:47 +0300

    cn: some changes

--- src/helpers/cn.ts
+++ src/helpers/cn.ts
@@ -2,13 +2,24 @@ interface IConditions {
   [modifier: string]: boolean | string;
 };
 
-export default (baseClass: string, conditions: IConditions, propsClass: string) => {
-  return baseClass + Object.keys(conditions).reduce((modifiers, modifier) => {
-    const value = conditions[modifier];
-    if (typeof value === 'boolean') {
-      return `${modifiers} ${baseClass}_${modifier}`;
-    }
+const cn = (baseClass: string, propsClasses?: string, conditions?: IConditions) => {
+  let finalClass = baseClass;
+  if (conditions) {
+    finalClass += Object.keys(conditions).reduce((modifiers, modifier) => {
+      const value = conditions[modifier];
+      if (typeof value === 'boolean') {
+        return `${modifiers} ${baseClass}_${modifier}`;
+      }
 
-    return `${modifiers} ${baseClass}_${modifier}_${value}`;
-  }, '') + ` ${propsClass}`;
+      return `${modifiers} ${baseClass}_${modifier}_${value}`;
+    }, '');
+  }
+
+  if (propsClasses) {
+    finalClass += ' ' + propsClasses;
+  }
+
+  return finalClass;
 };
+
+export default cn;
