11f1d4aa071d1e733f7aedde5b00e014633b4be2
Ilya Blinkov <blinkovilya@yandex-team.ru>
2019-10-10 23:42:21 +0300

    Firebase: Basic integration

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,21 @@ import { rootReducer, AppState, AppActions } from './store';
 import { Provider } from 'react-redux';
 import thunk, { ThunkMiddleware } from 'redux-thunk';
 
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCTkJ2p7ggiKkPU8MLvo7jJk3MsxmYc4FU',
+  authDomain: 'vkontaktlyah.firebaseapp.com',
+  databaseURL: 'https://vkontaktlyah.firebaseio.com',
+  projectId: 'vkontaktlyah',
+  storageBucket: 'vkontaktlyah.appspot.com',
+  messagingSenderId: '767305225250',
+  appId: '1:767305225250:web:58bd6ca2ae3ef14dc9267b'
+};
+
+firebase.initializeApp(firebaseConfig);
+
 const store = createStore(
   rootReducer,
   applyMiddleware(thunk as ThunkMiddleware<AppState, AppActions>)
