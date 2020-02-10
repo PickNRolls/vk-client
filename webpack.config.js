ea3acc458156c7812c5c2f2cacd1abf46ae0c54b
Ilya Blinkov <blinkovilya@yandex-team.ru>
2020-02-10 17:55:15 +0300

    ReactBuild * make project build stable * make hot-reload stable

--- webpack.config.js
+++ webpack.config.js
@@ -8,19 +8,43 @@ module.exports = {
     filename: 'bundle.js',
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js']
+    alias: {},
+    modules: [
+      path.resolve(__dirname, 'src'),
+      'node_modules',
+    ],
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.json', '.css', '.scss'],
+    enforceExtension: false,
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         exclude: /node_modules/,
-        use: ['babel-loader', 'ts-loader',],
+        use: [
+          'babel-loader',
+          'ts-loader',
+        ],
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
       },
+      {
+         test: /\.(png|svg|jpg|gif)$/,
+         use: [
+           'file-loader',
+         ],
+       },
     ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    compress: true,
+    port: 3000,
+    historyApiFallback: true,
   }
 };
