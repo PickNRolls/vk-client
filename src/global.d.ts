32107b459c860b095a3bc314db0a020f74612ce2
Ilya Blinkov <blinkovilya@yandex-team.ru>
2020-02-10 17:45:29 +0300

    ReactBuild * declare TypeScript images modules

--- src/global.d.ts
+++ src/global.d.ts
@@ -6,3 +6,12 @@ declare var require: {
     callback: (require: <T>(path: string) => T) => void
   ) => void;
 };
+
+/*
+  Instructions for TypeScript to use 'any'
+  for all file-module imports
+*/
+declare module '*.png';
+declare module '*.jpeg';
+declare module '*.jpg';
+declare module '*.svg';
